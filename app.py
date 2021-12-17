fe7f6cb7c5331364c059822e5aa1a2fb8c7b9723
Snehal-2310 <62506140+Snehal-2310@users.noreply.github.com>
2021-12-17 22:55:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,10 +29,8 @@ def predict():
         exang = request.form.get('exang')
         oldpeak = float(request.form['oldpeak'])
         slope = request.form.get('slope')
-        ca = int(request.form['ca'])
-        thal = request.form.get('thal')
         
-        data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+        data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope]])
         my_prediction = model.predict(data)
         
         return render_template('result.html', prediction=my_prediction)
